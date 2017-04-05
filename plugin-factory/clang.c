ec9184b52afff0f421bd3e13141a9c4aad27eb9e
Kaho Ng <ngkaho1234@gmail.com>
2017-04-05 18:46:30 +0800

    plugin-factory/clang.c: add support for in-scope tagging

--- plugin-factory/clang.c
+++ plugin-factory/clang.c
@@ -15,16 +15,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define _FILE_OFFSET_BITS 64
-
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
 #include <sys/types.h>
+#include <stdarg.h>
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
+#include <malloc.h>
 #ifdef STDC_HEADERS
 #include <stdlib.h>
 #endif
@@ -50,6 +50,137 @@ struct visit_args {
 	/* Parser parameter from global */
 	const struct parser_param	*param;
 };
+
+struct strbuf_s {
+	char	*sbuf;
+	int	sbufsize;
+};
+#define STRBUF_INITIALSIZE 80
+#define STRBUF_EXPANDSIZE 80
+
+/*
+ * Expand buffer so that afford to the length data at least
+ */
+static int			/* positive when success, -1 when short of memory */
+strbuf_expandbuf(
+	struct strbuf_s	*sb,	/* strbuf_s structure */
+	int		nslen)	/* new string length */
+{
+	char	*nsbuf;
+	int	nbufsize = nslen + 1;	/* To hold \0 */
+
+	if (sb->sbufsize >= nbufsize)
+		return sb->sbufsize;
+
+	nsbuf = (char *)realloc(sb->sbuf, nbufsize);
+	if (!nsbuf)
+		return -1;
+
+	sb->sbufsize = nbufsize;
+	sb->sbuf = nsbuf;
+
+	return sb->sbufsize;
+}
+
+/*
+ * Open string buffer
+ */
+static struct strbuf_s *	/* strbuf_s structure */
+strbuf_open(int init)		/* initial buffer size */
+{
+	struct strbuf_s	*sb;
+	char		*sbuf;
+	int		sbufsize;
+
+	sbufsize = (init > 0) ? init : STRBUF_INITIALSIZE;
+	sb = (struct strbuf_s *)calloc(sizeof(struct strbuf_s), 1);
+	if (!sb)
+		return NULL;
+	sbuf = (char *)malloc(sbufsize);
+	if (!sbuf) {
+		free(sb);
+		return NULL;
+	}
+
+	sb->sbufsize = sbufsize;
+	sb->sbuf = sbuf;
+
+	return sb;
+}
+
+/*
+ * Close string buffer
+ */
+static void
+strbuf_close(struct strbuf_s *sb)	/* strbuf_s structure */
+{
+	free(sb->sbuf);
+	free(sb);
+}
+
+/*
+ * Return the content of string buffer
+ */
+static char *				/* string */
+strbuf_value(struct strbuf_s *sb)	/* strbuf_s structure */
+{
+	int	endoffs;
+
+	endoffs = sb->sbufsize - 1;
+	sb->sbuf[endoffs] = 0;
+	return sb->sbuf;
+}
+
+/*
+ * Put string
+ */
+static void
+strbuf_puts(
+	struct strbuf_s	*sb,	/* strbuf_s structure */
+	const char	*s)	/* string */
+{
+	char	*p;
+
+	p = sb->sbuf;
+	strbuf_expandbuf(sb, strlen(s));
+
+	while (*s) {
+		*p++ = *s++;
+	}
+	*p = 0;
+}
+
+/*
+ * Do sprintf into string buffer
+ */
+static int
+strbuf_sprintf(
+	struct strbuf_s	*sb,	/* strbuf_s structure */
+	const char	*fmt,	/* format string */
+	...)			/* ARGS */
+{
+	va_list	ap;
+	int	ret;
+	int	slen;
+	int	sbufsize;
+
+	va_start(ap, fmt);
+	ret = slen = vsnprintf(NULL, 0, fmt, ap);
+	va_end(ap);
+	if (ret < 0)
+		goto out;
+
+	ret = sbufsize = strbuf_expandbuf(sb, slen);
+	if (ret < 0)
+		goto out;
+
+	va_start(ap, fmt);
+	ret = vsnprintf(sb->sbuf, sbufsize, fmt, ap);
+	va_end(ap);
+
+out:
+	return ret;
+}
 
 /*
  * Free the line buffer returned by getline()
@@ -101,10 +232,13 @@ is_definition(CXCursor cursor)	/* Cursor */
 	enum CXCursorKind	cursorkind;
 
 	cursorkind = clang_getCursorKind(cursor);
-	return clang_isCursorDefinition(cursor) &&
+	return (clang_isCursorDefinition(cursor) &&
 	    cursorkind != CXCursor_CXXAccessSpecifier &&
 	    cursorkind != CXCursor_TemplateTypeParameter &&
-	    cursorkind != CXCursor_UnexposedDecl;
+	    cursorkind != CXCursor_UnexposedDecl)
+		||
+	    (clang_isDeclaration(cursorkind) &&
+	     cursorkind == CXCursor_VarDecl);
 }
 
 /*
@@ -116,8 +250,7 @@ is_reference(CXCursor cursor)	/* Cursor */
 	enum CXCursorKind	cursorkind;
 
 	cursorkind = clang_getCursorKind(cursor);
-	return cursorkind == CXCursor_DeclRefExpr ||
-	    cursorkind == CXCursor_MemberRefExpr;
+	return cursorkind == CXCursor_DeclRefExpr;
 }
 
 /*
@@ -136,6 +269,83 @@ should_tag(CXCursor cursor)	/* Cursor */
 	linkagekind = clang_getCursorLinkage(refcursor);
 	return linkagekind != CXLinkage_Invalid &&
 	    linkagekind != CXLinkage_NoLinkage;
+}
+
+/*
+ * Prepend semantic parent to the content of a string buffer
+ */
+static void
+prepend_semantic_parent(
+	struct strbuf_s	*sb,		/* Cursor */
+	const char	*spspelling)	/* Spelling of semantic parent */
+{
+	char	*tsbuf;
+	int	tslen;
+
+	tslen = strlen(strbuf_value(sb));
+	tsbuf = malloc(tslen + 1);
+	tsbuf[tslen] = 0;
+
+	memcpy(tsbuf, strbuf_value(sb), tslen);
+	strbuf_sprintf(sb, "%s::%s", spspelling, tsbuf);
+
+	free(tsbuf);
+}
+
+/*
+ * Check if a cursor points to a named scope
+ */
+static int
+is_named_scope(CXCursor cursor)	/* Cursor */
+{
+	enum CXCursorKind	cursorkind;
+
+	cursorkind = clang_getCursorKind(cursor);
+	return cursorkind == CXCursor_Namespace ||
+	    cursorkind == CXCursor_StructDecl ||
+	    cursorkind == CXCursor_UnionDecl ||
+	    cursorkind == CXCursor_EnumDecl ||
+	    cursorkind == CXCursor_ClassDecl ||
+	    cursorkind == CXCursor_ClassTemplate ||
+	    cursorkind == CXCursor_ClassTemplatePartialSpecialization;
+}
+
+/*
+ * Prepend spelling of each semantic parents of a cursor
+ * to a string buffer
+ */
+static void
+fix_tag_semantic_parent(
+	CXCursor	cursor,
+	struct strbuf_s	*sb)
+{
+	CXCursor	spcursor;
+
+	spcursor = clang_getCursorSemanticParent(cursor);
+	while (!clang_Cursor_isNull(spcursor) &&
+	       is_named_scope(spcursor)) {
+		CXCursor	nspcursor;
+		CXString	spspellingstr;
+		const char	*spspelling;
+
+		spspellingstr = clang_getCursorSpelling(spcursor);
+		spspelling = clang_getCString(spspellingstr);
+
+		if (!spspelling)
+			spspelling = "";
+
+		prepend_semantic_parent(sb, spspelling);
+
+		clang_disposeString(spspellingstr);
+
+		/*
+		 * Find into the semantic parent even further if viable
+		 */
+		nspcursor = clang_getCursorSemanticParent(cursor);
+		if (clang_equalCursors(spcursor, nspcursor))
+			break;
+		spcursor = nspcursor;
+	}
 }
 
 /*
@@ -195,10 +405,11 @@ visit_children(
 	}
 	
 	if (should_tag(cursor)) {
-		ssize_t	nread;
-		ssize_t	linelen;
-		size_t	linebuflen = 0;
-		char	*linebuf = NULL;
+		ssize_t		nread;
+		ssize_t		linelen;
+		size_t		linebuflen = 0;
+		char		*linebuf = NULL;
+		struct strbuf_s	*sb;
 
 		/*
 		 * Fetch the required line from the source file
@@ -224,39 +435,52 @@ visit_children(
 			linebuf[nread] = 0;
 		}
 
+		sb = strbuf_open(0);
+		if (!sb) {
+			free_line_buf(linebuf);
+			goto out;
+		}
+
+
 		/*
 		 * Pass the tag information we gathered to Global
 		 */
 		if (is_definition(cursor)) {
+			strbuf_puts(sb, spellstr);
+			fix_tag_semantic_parent(cursor, sb);
+
 			fprintf(stderr,
 				"Definition [%s] [%u] [%s] [%s]\n",
-				spellstr,
+				strbuf_value(sb),
 				line,
 				pathstr,
 				linebuf);
 
 			param->put(PARSER_DEF,
-				   spellstr,
+				   strbuf_value(sb),
 				   line,
 				   pathstr,
 				   linebuf,
 				   param->arg);
 		} else if (is_reference(cursor)) {
+			strbuf_puts(sb, spellstr);
+
 			fprintf(stderr,
 				"Reference [%s] [%u] [%s] [%s]\n",
-				spellstr,
+				strbuf_value(sb),
 				line,
 				pathstr,
 				linebuf);
 
 			param->put(PARSER_REF_SYM,
-				   spellstr,
+				   strbuf_value(sb),
 				   line,
 				   pathstr,
 				   linebuf,
 				   param->arg);
 		}
 
+		strbuf_close(sb);
 		free_line_buf(linebuf);
 	}
 
